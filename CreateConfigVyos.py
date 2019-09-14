50bd86b7d0e8aeccd7c8dd97776906915c82ff22
GuillermoElectrico <guillermoelectrico@outlook.com>
2019-09-14 13:38:52 +0200

    add comments

--- CreateConfigVyos.py
+++ CreateConfigVyos.py
@@ -20,6 +20,7 @@ def iprange(ip,mask): #entrega una tupla con la primera y ultima ip del rango
 ############################################################################################
 
 # sacar configuración del yaml
+
 assert path.exists("Settings.yml"), 'Settings file not found: Settings.yml'
 new_map = yaml.load(open("Settings.yml"), Loader=yaml.FullLoader)
 PublicRange_yaml = new_map['PublicRange']
@@ -27,6 +28,7 @@ PrivateRange_yaml = new_map['PrivateRange']
 RuleInitNumber_yaml = int(new_map['RuleInitNumber'])
 
 # crear rangos públicos disponibles y configuración vyos (interfaces y nat)
+
 PublicsIP = dict()
 list = 0 # mapping list to id
 
@@ -41,7 +43,11 @@ for subnet in PublicRange_yaml:
     PublicsIP[list]['Gateway'] = ip2long(subnet['Gateway']) # guardamos lista de IP,s no usables 
     PublicsIP[list]['Interface'] = subnet['Interface'] # guardamos interface subnet pública
     
+# creamos/sobreescribimos archivo de texto donde se guardarán todos los comandos
+    
 f = open("commands.txt","w+")
+
+# para evitar advertencias durante el commit, primero ingresar direcciones ip disponibles en interfaces ethernet.
 
 for i in range (1, list+1):
     for x in PublicsIP[i]['range']:
@@ -52,15 +58,9 @@ for i in range (1, list+1):
         if PublicsIP[i]['Gateway'] == x:
             discard = True
         if discard == False:
-#            print (long2ip(x))
-#            ruleNumber = ruleNumber + 1
             f.write ("set interfaces ethernet {} address '{}/{}'\n" .format(PublicsIP[list]['Interface'], long2ip(x), PublicsIP[list]['MaskBits']))
-#            f.write ("set nat destination rule {} destination address '{}'\n" .format(ruleNumber, long2ip(x)))
-#            f.write ("set nat destination rule {} inbound-interface '{}'\n" .format(ruleNumber,PublicsIP[list]['Interface']))
-#            f.write ("set nat source rule {} outbound-interface '{}'\n" .format(ruleNumber,PublicsIP[list]['Interface']))
-#            f.write ("set nat source rule {} translation address '{}'\n" .format(ruleNumber, long2ip(x)))
 
-# Creamos rango privado y asociamos IP pública disponible con IP privada (hasta acabar IP,s públicas) y crear configuración vyos
+# sacamos listado IP privada y ingresamos ip puerta de enlace en la interfaz configurada.
 
 discardLanIP = dict()
 
@@ -72,6 +72,7 @@ for lannet in PrivateRange_yaml:
     gatewayLan = ip2long(lannet['Gateway'])
     f.write ("set interfaces ethernet {} address '{}/{}'\n" .format(lannet['Interface'], lannet['Gateway'], lannet['MaskBits']))
     
+# Creamos reglas de nat con todas las IP,s públicas disponibles.
 
 ruleNumber = RuleInitNumber_yaml - 1
 
@@ -84,14 +85,13 @@ for i in range (1, list+1):
         if PublicsIP[i]['Gateway'] == x:
             discard = True
         if discard == False:
-#            print (long2ip(x))
             ruleNumber = ruleNumber + 1
-#            f.write ("set interfaces ethernet {} address '{}/{}'\n" .format(PublicsIP[list]['Interface'], long2ip(x), PublicsIP[list]['MaskBits']))
             f.write ("set nat destination rule {} destination address '{}'\n" .format(ruleNumber, long2ip(x)))
             f.write ("set nat destination rule {} inbound-interface '{}'\n" .format(ruleNumber,PublicsIP[list]['Interface']))
             f.write ("set nat source rule {} outbound-interface '{}'\n" .format(ruleNumber,PublicsIP[list]['Interface']))
             f.write ("set nat source rule {} translation address '{}'\n" .format(ruleNumber, long2ip(x)))
     
+# Creamos rango privado y asociamos IP pública disponible con IP privada (hasta acabar IP,s públicas) 
  
 ruleLanNumber = RuleInitNumber_yaml - 1
 
@@ -105,13 +105,16 @@ for r in rangeLan:
         if discard == False:
             ruleLanNumber = ruleLanNumber + 1
             if ruleLanNumber <= ruleNumber:
-    #            print (long2ip(r))
                 f.write ("set nat destination rule {} translation address '{}'\n" .format(ruleLanNumber, long2ip(r)))
                 f.write ("set nat source rule {} source address '{}'\n" .format(ruleLanNumber, long2ip(r)))
+                
+# Creamos ruta estática por defecto a las puertas de enlace públicas disponibles
 
 for i in range (1, list+1):
     f.write ("set protocols static route 0.0.0.0/0 next-hop '{}'\n" .format(long2ip(PublicsIP[i]['Gateway'])))
     
+# cerramos archivo y finalizamos  
+   
 f.close() 
 
 print ("OK")
